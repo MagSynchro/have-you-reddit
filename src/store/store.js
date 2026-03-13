6d82b3e78b6d65a91880a406a4b6c5eb7b4b0e79
Phillip Abernathy <moraff@gmail.com>
2026-03-13 06:34:28 -0400

    code cleanup as reducing scope of slices.

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // import reducers
 import subredditsReducer from "../features/subreddits/subredditsSlice.js";
 import postsReducer from "../features/posts/postsSlice.js";
-import commentsReducer from "../features/comments/commentsSlice.js";
 
 
 export default configureStore({
   reducer: {
     subreddits: subredditsReducer,
-    posts: postsReducer,
-    comments: commentsReducer,
+    posts: postsReducer,    
 
   },
 });
